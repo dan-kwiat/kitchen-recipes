ffe8e5a638ad4a0d3c1df6121d6cec9a26b455e6
dan-kwiat <dan.kwiat@gmail.com>
2020-03-24 12:58:03 +0000

    feat(pills): stretch pills to take up full width

--- components/pills.js
+++ components/pills.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types'
 const Pills = ({ items, selected, setSelected }) => {
   return (
     <ul className="flex">
-      {items.map(x => {
+      {items.map((x, i) => {
         const dynamicClassNames = selected === x.id ? (
           'border-blue-500 bg-blue-500 text-white'
         ) : (
           'border-white hover:border-gray-200 text-blue-500 hover:bg-gray-200'
         )
+        const marginClassName = i < items.length - 1 ? 'mr-2' : ''
         return (
-          <li key={x.id} className="mr-3">
+          <li key={x.id} className={`flex-1 ${marginClassName}`}>
             <span
-              className={`inline-block border rounded py-1 px-3 cursor-pointer ${dynamicClassNames}`}
+              className={`text-center block border rounded py-2 px-3 cursor-pointer ${dynamicClassNames}`}
               onClick={() => setSelected(x.id)}
             >
               {x.label}
