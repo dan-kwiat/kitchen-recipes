44bd0cea65532ac691c2c29d2661052a86a8cfb4
dan-kwiat <dan.kwiat@gmail.com>
2020-03-25 21:33:33 +0000

    feat(swipe): add glowing left/right buttons to swipe component

--- pages/chana-masala.js
+++ pages/chana-masala.js
@@ -29,8 +29,20 @@ export default () => {
         className='w-full m-0 p-0'
         selected={-1}
         growthFactor={0}
-        renderButtonLeft={null}
-        renderButtonRight={null}
+        renderButtonLeft={props => {
+          return (
+            <button className={`lg:hidden absolute top-0 left-0 p-2 focus:outline-none ${props.disabled ? 'opacity-25 cursor-not-allowed' : 'breathe-opacity'}`} {...props}>
+              <svg className={`fill-current text-teal-500 ${props.disabled ? '' : 'hover:text-teal-700'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/></svg>
+            </button>
+          )
+        }}
+        renderButtonRight={props => {
+          return (
+            <button className={`lg:hidden absolute top-0 right-0 p-2 focus:outline-none ${props.disabled ? 'opacity-25 cursor-not-allowed' : 'breathe-opacity'}`} {...props}>
+              <svg className={`fill-current text-teal-500 ${props.disabled ? '' : 'hover:text-teal-700'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
+            </button>
+          )
+        }}
         buttonScrollPercent={100}
         snap={(x, velocity, selected) => {
           return Math.round((x + velocity*150)/100)*100
@@ -42,7 +54,7 @@ export default () => {
           const Component = x.component
           return (
             <div key={x.id} className={`overflow-visible w-full inline-block align-top lg:block lg:px-10 ${selected === x.id ? '' : 'lg:hidden'}`} >
-              <div className='lg:hidden text-center block py-2 px-3 text-blue-500'>
+              <div className='lg:hidden text-center block py-2 px-3'>
                 {x.label.toUpperCase()}
               </div>
               <Component />
