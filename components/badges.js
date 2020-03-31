ebbab5729a5f357913f6c7ae9638602d4a1b5fcf
dan-kwiat <dan.kwiat@gmail.com>
2020-03-31 09:35:21 +0100

    refactor(badges): render svg icons inline

--- components/badges.js
+++ components/badges.js
@@ -1,29 +1,33 @@
-const Badge = ({ icon, alt, value, children }) => {
+const Badge = ({ children }) => {
   return (
-    <div className='px-3 py-2 my-1 mr-3 inline-block bg-gray-100 rounded-full border border-gray-200'>
-      <img
-        className='inline-block align-top mr-2'
-        src={icon}
-        alt={alt}
-      />
-      {value}
-    </div>
+    <div
+      className='px-3 py-2 my-1 mr-3 leading-6 inline-block bg-gray-100 text-gray-800 rounded-full border border-gray-200'
+      children={children}
+    />
+  )
+}
+
+const Icon = ({ children }) => {
+  return (
+    <span className='inline-block w-6 h-6 align-top mr-2 fill-current' children={children} />
   )
 }
 
 const Badges = ({ quantity, time }) => {
   return (
     <div className='my-4'>
-      <Badge
-        icon='/static/icons/people.svg'
-        alt='People Icon'
-        value={quantity}
-      />
-      <Badge
-        icon='/static/icons/clock.svg'
-        alt='Clock Icon'
-        value={time}
-      />
+      <Badge>
+        <Icon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/></svg>
+        </Icon>
+        {quantity}
+      </Badge>
+      <Badge>
+        <Icon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>
+        </Icon>
+        {time}
+      </Badge>
     </div>
   )
 }
