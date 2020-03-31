c6b225ccf33a0aa38b7126678c1625d7eaaf85ad
dan-kwiat <dan.kwiat@gmail.com>
2020-03-31 09:11:53 +0100

    feat(swipe): animate glowing swipe arrows

--- pages/chana-masala.js
+++ pages/chana-masala.js
@@ -18,6 +18,7 @@ export default () => {
         quantity='4 servings'
         time='45m'
       />
+      <hr />
       <div className='hidden lg:block'>
         <Pills
           items={SECTIONS}
@@ -29,17 +30,17 @@ export default () => {
         className='w-full m-0 p-0'
         selected={-1}
         growthFactor={0}
-        renderButtonLeft={props => {
+        renderButtonRight={props => {
           return (
-            <button className={`lg:hidden absolute top-0 left-0 p-2 focus:outline-none ${props.disabled ? 'opacity-25 cursor-not-allowed' : 'breathe-opacity'}`} {...props}>
-              <svg className={`fill-current text-teal-500 ${props.disabled ? '' : 'hover:text-teal-700'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/></svg>
+            <button className={`w-16 lg:hidden flex flex-row-reverse absolute top-0 right-0 p-2 focus:outline-none ${props.disabled ? 'opacity-25 cursor-not-allowed' : ''}`} {...props}>
+              <svg className={`fill-current text-teal-500 ${props.disabled ? '' : 'hover:text-teal-700 breathe-opacity left'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/></svg>
             </button>
           )
         }}
-        renderButtonRight={props => {
+        renderButtonLeft={props => {
           return (
-            <button className={`lg:hidden absolute top-0 right-0 p-2 focus:outline-none ${props.disabled ? 'opacity-25 cursor-not-allowed' : 'breathe-opacity'}`} {...props}>
-              <svg className={`fill-current text-teal-500 ${props.disabled ? '' : 'hover:text-teal-700'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
+            <button className={`w-16 lg:hidden absolute top-0 left-0 p-2 focus:outline-none ${props.disabled ? 'opacity-25 cursor-not-allowed' : ''}`} {...props}>
+              <svg className={`fill-current text-teal-500 ${props.disabled ? '' : 'hover:text-teal-700 breathe-opacity right'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
             </button>
           )
         }}
