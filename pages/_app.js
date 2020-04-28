083f6de38dc5a155ded189ffb76b293cd729372a
dan-kwiat <dan.kwiat@gmail.com>
2020-04-28 21:30:07 +0100

    feat(checkbox): add className form-checkbox

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,21 @@ import Nav from '../components/nav'
 import '@rmwc/tooltip/tooltip.css'
 import '../index.css'
 
-let listItemId = 0
-
 const mdComponents = {
   li: props => {
-    console.log(props)
-    listItemId++
     return (
-      <div class='flex items-start mt-4'>
-        <div class='flex items-center'>
-          &#8203;
-          <input id={listItemId} type='checkbox' class='form-checkbox border-gray-400 h-4 w-4 text-teal-500 cursor-pointer' />
-        </div>
-        <label for={listItemId} class='ml-3 text-gray-700 cursor-pointer'>{props.children}</label>
-      </div>
+      <li className='mb-4'>
+        <label className='flex items-start cursor-pointer no-tap-highlight'>
+          <input
+            type='checkbox'
+            className='form-checkbox bg-white text-teal-500 border-gray-400 focus:border-teal-500 cursor-pointer'
+            style={{ marginTop: 4 }}
+          />
+          <span className='task-text ml-2 text-gray-700'>
+            {props.children}
+          </span>
+        </label>
+      </li>
     )
   }
 }
