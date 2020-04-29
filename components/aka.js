401415c1580841ad6b74267e737ee88d087864ab
dan-kwiat <dan.kwiat@gmail.com>
2020-04-29 17:07:13 +0100

    refactor(aka): show first item instead of child

--- components/aka.js
+++ components/aka.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types'
 import { Tooltip } from '@rmwc/tooltip'
 import { DeviceContext } from '../context'
 
-const Aka = ({ children, terms }) => {
+const Aka = ({ terms }) => {
   const { touch } = useContext(DeviceContext)
-  const isString = typeof terms === 'string'
   return (
     <Tooltip
       className='text-base'
       content={
         <ul className='p-2 max-w-xs'>
-          {(isString ? [terms] : terms).map((x, i) => (
-            <li key={i} className={(isString || i === terms.length - 1) ? '' : 'pb-2'}>{x}</li>
+          {terms.slice(1).map((x, i) => (
+            <li key={i} className={(i === terms.length - 2) ? '' : 'pb-2'}>
+              {x}
+            </li>
           ))}
         </ul>
       }
@@ -21,16 +22,15 @@ const Aka = ({ children, terms }) => {
       <span
         className={`py-1 font-semibold border-b-2 border-yellow-500 hover:bg-yellow-500 transition-colors duration-300`}
       >
-        {children}
+        {terms[0]}
       </span>
     </Tooltip>
   )
 }
 Aka.propTypes = {
-  terms: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.string),
-    PropTypes.string,
-  ]).isRequired,
+  terms: PropTypes.arrayOf(
+    PropTypes.string.isRequired
+  ).isRequired,
 }
 
 export default Aka
