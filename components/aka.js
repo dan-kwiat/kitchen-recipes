192dcfeb1b9a2caca835f802758d95fb372a94e5
dan-kwiat <dan.kwiat@gmail.com>
2020-04-29 16:51:24 +0100

    feat(aka): make activation method conditional on touch device

--- components/aka.js
+++ components/aka.js
@@ -1,24 +1,36 @@
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
 import { Tooltip } from '@rmwc/tooltip'
+import { DeviceContext } from '../context'
 
 const Aka = ({ children, terms }) => {
+  const { touch } = useContext(DeviceContext)
+  const isString = typeof terms === 'string'
   return (
     <Tooltip
       className='text-base'
       content={
         <ul className='p-2 max-w-xs'>
-          {terms.map((x, i) => (
-            <li key={i} className={i < terms.length-1 ? 'pb-2' : ''}>{x}</li>
+          {(isString ? [terms] : terms).map((x, i) => (
+            <li key={i} className={(isString || i === terms.length - 1) ? '' : 'pb-2'}>{x}</li>
           ))}
         </ul>
       }
+      activateOn={touch ? 'click' : 'hover'}
     >
       <span
-        className={`p-1 font-semibold border-b-2 border-yellow-500 hover:bg-yellow-500`}
+        className={`py-1 font-semibold border-b-2 border-yellow-500 hover:bg-yellow-500 transition-colors duration-300`}
       >
         {children}
       </span>
     </Tooltip>
   )
+}
+Aka.propTypes = {
+  terms: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.string,
+  ]).isRequired,
 }
 
 export default Aka
