26bcc3fb1f0274906b5aac0837cfd6d3d3b4f170
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2022-10-05 13:27:40 +0100

    feat(styles): add padding on mobile

--- pages/banana-bread.js
+++ pages/banana-bread.js
@@ -2,7 +2,7 @@ import Content from "../components/banana-bread.md"
 
 export default function Page() {
   return (
-    <div className="prose">
+    <div className="px-4 lg:px-0 pb-12 prose">
       <Content />
     </div>
   )
