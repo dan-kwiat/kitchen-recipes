2bddf422225a0a25f301c5fb57b3584d2b3fc1aa
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2024-06-07 19:31:23 +0100

    feat(recipe): re-add sourdough loaf

---
+++ pages/sourdough-loaf.js
@@ -0,0 +1,9 @@
+import Content from "../components/sourdough-loaf.mdx"
+
+export default function Page() {
+  return (
+    <div className="px-4 lg:px-0 pb-12 prose">
+      <Content />
+    </div>
+  )
+}
